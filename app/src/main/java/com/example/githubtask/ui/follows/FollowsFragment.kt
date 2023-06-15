a6b2b9b198dd07bd93c4203f905de219acec7184
burakselcuk1 <design.burakk@gmail.com>
2023-06-15 21:32:52 +0300

    Get bundle data

--- app/src/main/java/com/example/githubtask/ui/follows/FollowsFragment.kt
+++ app/src/main/java/com/example/githubtask/ui/follows/FollowsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.githubtask.ui.follows
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import com.example.githubtask.R
 import com.example.githubtask.base.BaseFragment
 import com.example.githubtask.databinding.FragmentFollowsBinding
+import com.example.githubtask.model.GithubUser
 
 
 class FollowsFragment : BaseFragment<FragmentFollowsBinding, FollowsFragmentViewModes>(
@@ -16,5 +18,10 @@ class FollowsFragment : BaseFragment<FragmentFollowsBinding, FollowsFragmentView
 ) {
     override fun onInitDataBinding() {
 
+        arguments?.let {
+            val user = it.getSerializable("user") as GithubUser
+            // İşlem yapmak için 'user' nesnesini kullanabilirsiniz
+            Log.d("burak",user.following.toString())
+        }
     }
-}
+    }
