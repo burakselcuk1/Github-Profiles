d821d1147081fd58f3e6f88c6c62c4915f48de49
burakselcuk1 <design.burakk@gmail.com>
2023-06-16 02:31:39 +0300

    Showing in ui followers seledted user in UserDetailFragment

--- app/src/main/java/com/example/githubtask/ui/userDetail/UserDetailFragment.kt
+++ app/src/main/java/com/example/githubtask/ui/userDetail/UserDetailFragment.kt
@@ -1,13 +1,16 @@
 package com.example.githubtask.ui.userDetail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.githubtask.R
 import com.example.githubtask.base.BaseFragment
 import com.example.githubtask.databinding.FragmentUserDetailBinding
+import com.example.githubtask.model.GithubUser
 
 
 class UserDetailFragment : BaseFragment<FragmentUserDetailBinding, UserDetailViewModel>(
@@ -15,5 +18,23 @@ class UserDetailFragment : BaseFragment<FragmentUserDetailBinding, UserDetailVie
     viewModelClass = UserDetailViewModel::class.java
 ) {
     override fun onInitDataBinding() {
+        arguments?.let {
+            val user = it.getParcelable("userSelected") as? GithubUser
+            if(user != null) {
+                // İşlem yapmak için 'user' nesnesini kullanabilirsiniz
+                Log.d("burak", user.following.toString())
+
+                with(binding) {
+                    followUserName.text = user.login
+                    followBio.text = user.bio
+                    Glide.with(requireActivity())
+                        .load(user.avatarUrl)
+                        .into(followUserImage)
+                }
+            } else {
+                // Handle null user scenario
+                Log.d("burak", "User is null")
+            }
+        }
     }
 }
